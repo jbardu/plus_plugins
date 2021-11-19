7dbb01676e0671c8de600545c39de6466055f3dd
justin <justin@mbp16inch2021.lan>
2021-11-19 18:04:25 +1100

    magic 9

--- packages/sensors_plus/sensors_plus/ios/Classes/FLTSensorsPlusPlugin.m
+++ packages/sensors_plus/sensors_plus/ios/Classes/FLTSensorsPlusPlugin.m
@@ -67,6 +67,27 @@ static void sendTriplet(Float64 x, Float64 y, Float64 z, FlutterEventSink sink) 
   sink([FlutterStandardTypedData typedDataWithFloat64:event]);
 }
 
+static void sendMat(GLKMatrix4 m, FlutterEventSink sink) {
+  NSMutableData* event = [NSMutableData dataWithCapacity:16 * sizeof(float)];
+  [event appendBytes:&m.m00 length:sizeof(float)];
+  [event appendBytes:&m.m01 length:sizeof(float)];
+  [event appendBytes:&m.m02 length:sizeof(float)];
+  [event appendBytes:&m.m03 length:sizeof(float)];
+  [event appendBytes:&m.m10 length:sizeof(float)];
+  [event appendBytes:&m.m11 length:sizeof(float)];
+  [event appendBytes:&m.m12 length:sizeof(float)];
+  [event appendBytes:&m.m13 length:sizeof(float)];
+  [event appendBytes:&m.m20 length:sizeof(float)];
+  [event appendBytes:&m.m21 length:sizeof(float)];
+  [event appendBytes:&m.m22 length:sizeof(float)];
+  [event appendBytes:&m.m23 length:sizeof(float)];
+  [event appendBytes:&m.m30 length:sizeof(float)];
+  [event appendBytes:&m.m31 length:sizeof(float)];
+  [event appendBytes:&m.m32 length:sizeof(float)];
+  [event appendBytes:&m.m33 length:sizeof(float)];
+  sink([FlutterStandardTypedData typedDataWithFloat32:event]);
+}
+
 @implementation FLTAccelerometerStreamHandlerPlus
 
 - (FlutterError*)onListenWithArguments:(id)arguments eventSink:(FlutterEventSink)eventSink {
@@ -152,7 +173,7 @@ static void sendTriplet(Float64 x, Float64 y, Float64 z, FlutterEventSink sink) 
 
 @end
 
-	// motionManager.AccelerometerUpdateInterval = 0.01; // 100Hz
+// motionManager.AccelerometerUpdateInterval = 0.01; // 100Hz
 
 @implementation FLTMagicStreamHandlerPlus
 
@@ -162,31 +183,65 @@ static void sendTriplet(Float64 x, Float64 y, Float64 z, FlutterEventSink sink) 
                                                       toQueue: [[NSOperationQueue alloc] init]
                                       		  withHandler:^(CMDeviceMotion* motion, NSError* error) {
 
-	 				//CMRotationRate AA = motion.rotationRate;
-	 				//CMAcceleration BB = motion.gravity;
-	 				//CMAcceleration CC = motion.userAcceleration;
+					  if (error) {
 
-	 				CMAttitude *DD = motion.attitude;
-					//CMAttitude *attitude = motion.attitude;
-        				//CMRotationMatrix rm = attitude.rotationMatrix;
+					  } else {
+// https://math.stackexchange.com/questions/1637464/find-unit-vector-given-roll-pitch-and-yaw
+						float aspect = fabsf(self.view.bounds.size.width / self.view.bounds.size.height);
+						GLKMatrix4 projectionMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(45.0f), aspect, 0.1f, 100.0f);
 
-//Describes a reference frame in which the Z axis is vertical and the X axis points toward true north. Note that using this reference frame may require device movement to calibrate the magnetometer. It also requires the location to be available in order to calculate the difference between magnetic and true north
-                                        // CMMagneticField magneticField = magData.magneticField;
+						CMRotationMatrix r = self.motionManager.deviceMotion.attitude.rotationMatrix;
+						float elevation = fabsf(self.motionManager.deviceMotion.attitude.roll);
+
+						GLKMatrix4 camFromIMU = GLKMatrix4Make(r.m11, r.m12, r.m13, 0,
+										       r.m21, r.m22, r.m23, 0,
+										       r.m31, r.m32, r.m33, 0,
+										       0,     0,     0,     1);
+
+						GLKMatrix4 viewFromCam = GLKMatrix4Translate(GLKMatrix4Identity, 0, 0, 0);
+						GLKMatrix4 imuFromModel = GLKMatrix4Identity;
+						GLKMatrix4 viewModel = GLKMatrix4Multiply(imuFromModel, GLKMatrix4Multiply(camFromIMU, viewFromCam));
+						bool isInvertible;
+						GLKMatrix4 modelView = GLKMatrix4Invert(viewModel, &isInvertible);
+
+						int viewport[4];
+						viewport[0] = 0.0f;
+						viewport[1] = 0.0f;
+						viewport[2] = self.view.frame.size.width;
+						viewport[3] = self.view.frame.size.height;
+
+						bool success;
+
+						//
+						// assume center of the view
+						//
+						GLKVector3 vector3 = GLKVector3Make(self.view.frame.size.width/2, self.view.frame.size.height/2, 1.0);     
+						GLKVector3 calculatedPoint = GLKMathUnproject(vector3, modelView, projectionMatrix, viewport, &success);
+
+						if(success) {
+						    //
+						    // CMAttitudeReferenceFrameXTrueNorthZVertical always point x to true north
+						    // with that, -y become east in 3D world
+						    //
+						    float angleInRadian = atan2f(-calculatedPoint.y, calculatedPoint.x);
+						    camFromIMU.m32 = angleInRadian;
+						    camFromIMU.m33 = elevation;
+					  	}
+						sendMat(camFromIMU, eventSink);
+//
+//Describes a reference frame in which the Z axis is vertical 
+//and the X axis points toward true north. 
+//Note that using this reference frame may require device movement 
+//to calibrate the magnetometer. It also requires the location to be 
+//available in order to calculate the difference between magnetic and true north
+// 
 // a device's original orientation is lying flat on a table, with the bottom of the device facing the user:
 //Change:	Rotation Around:	Caused By:
 //+Yaw	Z	The device is rotated counter-clockwise without lifting any edges.
 //+Pitch	X	The device is rotated towards its bottom.
 //+Roll	Y	The device is rotated towards its right side.
-
-                                        sendTriplet(DD.pitch, DD.roll, DD.yaw, eventSink);
-
                                       }];
 
-  //[_motionManager startMagnetometerUpdatesToQueue:[[NSOperationQueue alloc] init]
-  //                                    withHandler:^(CMMagnetometerData* magData, NSError* error) {
-  //                                     CMMagneticField magneticField = magData.magneticField;
-  //                                      sendTriplet(magneticField.x, magneticField.y,
-  //                                                  magneticField.z, eventSink);
   return nil;
 }
 
