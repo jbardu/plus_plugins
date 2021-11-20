296936fcca949f33b94156f4c304bd79d9c201ab
justin <justin@mbp16inch2021.lan>
2021-11-21 01:02:33 +1100

    “..”

--- packages/sensors_plus/sensors_plus_platform_interface/lib/src/method_channel_sensors.dart
+++ packages/sensors_plus/sensors_plus_platform_interface/lib/src/method_channel_sensors.dart
@@ -83,10 +83,9 @@ class MethodChannelSensors extends SensorsPlatform {
         _magicEventChannel.receiveBroadcastStream().map((dynamic event) {
       final list = event.cast<double>();
       return MagicEvent(
-			list[0]!, list[1]!, list[2]!, list[3]!,
-			list[4]!, list[5]!, list[6]!, list[7]!,
-			list[8]!, list[9]!, list[10]!, list[11]!,
-			list[12]!, list[13]!, list[14]!, list[15]!);
+			list[0]!, list[1]!, list[2]!,
+			list[3]!, list[4]!, list[5]!,
+			list[6]!, list[7]!, list[8]!);
     });
     return _magicEvents!;
   }
