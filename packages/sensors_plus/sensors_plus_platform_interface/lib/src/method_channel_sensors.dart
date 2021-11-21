6dc15242ca97fcfca5798c1cf8557db3f05c0719
justin <justin@mbp16inch2021.lan>
2021-11-21 11:53:23 +1100

    “..”

--- packages/sensors_plus/sensors_plus_platform_interface/lib/src/method_channel_sensors.dart
+++ packages/sensors_plus/sensors_plus_platform_interface/lib/src/method_channel_sensors.dart
@@ -85,7 +85,7 @@ class MethodChannelSensors extends SensorsPlatform {
       return MagicEvent(
 			list[0]!, list[1]!, list[2]!,
 			list[3]!, list[4]!, list[5]!,
-			list[6]!, list[7]!, list[8]!);
+			list[6]!, list[7]!, list[8]!, list[9]!);
     });
     return _magicEvents!;
   }
